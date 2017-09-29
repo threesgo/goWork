956995a185c16b2ecec25c543ad662f97b16cb4c
fangyibin <549500581@qq.com>
2017-09-30 01:22:33 +0800

    dddddddddddd

--- yun/src/main/java/com/yunwang/action/SysUserAction.java
+++ yun/src/main/java/com/yunwang/action/SysUserAction.java
@@ -33,9 +33,7 @@ public class SysUserAction extends AbstractLoginAction{
 	public String updateUserRoleDefault(){
 		try{
 			sysUserService.updateUserRoleDefault(sessionAdm.getId(),roleId);
-			success("更新默认角色成功!");
 			return success("更新默认角色成功!");
-			
 		}catch(Exception e){
 			LOG.error(e.getMessage());
 			return error("更新默认角色失败!");
