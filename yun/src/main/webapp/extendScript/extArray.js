85f736cb02c24dd178f89c5a666b7a684095e52d
549500581@qq.com <549500581@qq.com>
2017-12-15 15:06:43 +0800

    fyb

--- yun/src/main/webapp/extendScript/extArray.js
+++ yun/src/main/webapp/extendScript/extArray.js
@@ -126,3 +126,12 @@ if(!Array.prototype.indexOf){
         return -1;  
     };  
 }
+
+Array.prototype.remove = function(val) {
+	var index = this.indexOf(val);
+	if (index > -1) {
+		this.splice(index, 1);
+	}
+};
+
+
