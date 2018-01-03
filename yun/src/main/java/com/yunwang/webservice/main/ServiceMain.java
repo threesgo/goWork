6356ba6f7e8cbf9de6ff162a620d42904a0b2b86
YBF <YBF@YBF-PC>
2018-01-03 21:52:40 +0800

    fyb

--- yun/src/main/java/com/yunwang/webservice/main/ServiceMain.java
+++ yun/src/main/java/com/yunwang/webservice/main/ServiceMain.java
@@ -1,17 +1,10 @@
 package com.yunwang.webservice.main;
 
-import javax.xml.ws.Endpoint;
-
-import org.apache.cxf.jaxws.EndpointImpl;
-
-import com.yunwang.webservice.HandleMemberService;
-import com.yunwang.webservice.auth.AuthInterceptor;
-import com.yunwang.webservice.impl.HandleMemberServiceImpl;
 
 public class ServiceMain {
 	public static void main(String[] args) {
-		HandleMemberService handleMember = new HandleMemberServiceImpl();
-		EndpointImpl sd=(EndpointImpl) Endpoint.publish("http://192.168.1.115:8888/yun/HandleMemberService", handleMember);
+		//HandleMemberService handleMember = new HandleMemberServiceImpl();
+		//EndpointImpl sd=(EndpointImpl) Endpoint.publish("http://192.168.1.115:8888/yun/HandleMemberService", handleMember);
 		//添加拦截器，检查用户名和密码是否正确
 		//sd.getInInterceptors().add(new AuthInterceptor());
 		System.out.println("成功");
